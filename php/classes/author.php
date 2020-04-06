bbaaa5888a92fb9d4e0c52fcb7421b459cd09860
Russell Dorgan <clambakedbenz@gmail.com>
2020-04-06 12:05:19 -0600

    added code for: autoload.php, ValidateDate.php, ValidateUuid.php

--- php/classes/author.php
+++ php/classes/author.php
@@ -1,1 +1,14 @@
 <?php
+
+class Author {
+	var $authorId;
+	var $authorActivationToken;
+	var $authorAvatarUrl;
+	var $authorEmail;
+	var $authorHash;
+	var $authorUsername;
+	var $authorEmail;
+	var $authorUsername;
+	var $authorId;
+
+}
