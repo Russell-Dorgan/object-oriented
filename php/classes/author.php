c676d11ef6bc715bc2b8d688471ab962e9011ad3
Russell Dorgan <clambakedbenz@gmail.com>
2020-04-06 16:31:25 -0600

    fixing getters and setters content and formatting

--- php/classes/author.php
+++ php/classes/author.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace RussellDorgan\ObjectOriented;
 class Author {
 	private $authorId;
 	private $authorActivationToken;
@@ -9,15 +9,50 @@ class Author {
 	private $authorUsername;
 
 	public function __construct($newAuthorId, $newAuthorActivationToken, $newAuthorAvatarUrl, $newAuthorEmail, $newAuthorHash, $newAuthorUsername) {
+		$this->setAuthorId($newAuthorId);
+		$this->setAuthorActivationToken($newAuthorActivationToken);
+		$this->setAuthorAvatarUrl($newAuthorAvatarUrl);
+		$this->setAuthorEmail($newAuthorEmail);
+		$this->setAuthorHash($newAuthorHash);
+		$this->setAuthorUsername($newAuthorUsername);
 
 	}
 
-	public function getAuthorId(){
-	return $this->authorId;
-	}
-	public function setAuthorId($newAuthorId){
-		$this->authorId = $newAuthorId;
-	}
+		public function getAuthorId(){
+			return $this->authorId;
+		}
+		public function setAuthorId($newAuthorId){
+			$this->authorId = $newAuthorId;
+		}
+		public function getAuthorActivationToken(){
+			return $this->authorActivationToken;
+		}
+		public function setAuthorActivationToken($newAuthorActivationToken){
+			$this->authorActivationToken = $newAuthorActivationToken;
+		}
+		public function getAuthorAvatarUrl(){
+			return $this->authorAvatarUrl;
+		}
+		public function setAuthorAvatarUrl($newAuthorAvatarUrl){
+			$this->authorAvatarUrl = $newAuthorAvatarUrl;
+		}
+		public function getAuthorEmail(){
+			return $this->authorEmail;
+		}
+		public function setAuthorEmail($newAuthorEmail){
+			$this->authorEmail = $newAuthorEmail;
+		}
+		public function getAuthorHash(){
+			return $this->authorHash;
+		}
+		public function setAuthorHash($newAuthorHash){
+			$this->authorHash = $newAuthorHash;
+		}
+		public function getAuthorUsername(){
+			return $this->authorUsername;
+		}
+		public function setAuthorUsername($newAuthorUsername){
+			$this->authorUsername = $newAuthorUsername;
+		}
 
 }
-
--- php/lib/author.php
+++ php/lib/author.php
@@ -1,2 +1,8 @@
 <?php
-
+require_once(dirname(__DIR__, 1) . "/classes/Author.php");
+// The pdo object has been created for you.
+//require_once("/etc/apache2/capstone-mysql/Secrets.php");
+//$secrets =  new Secrets("/etc/apache2/capstone-mysql/cohort28/cfiniello.ini");
+//$pdo = $secrets->getPdoObject();
+use RussellDorgan\ObjectOriented\Author;
+$author = new Author(newAuthorId, newAuthorActivationToken, newAuthorAvatarUrl, newAuthorEmail, newauthorHash, newAuthorUsername);
