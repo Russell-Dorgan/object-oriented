5212d7b505cbc84f29f45220dbcb082b586babbd
Russell Dorgan <clambakedbenz@gmail.com>
2020-04-06 17:07:07 -0600

    fixing getters and setters content and formatting

--- php/classes/author.php
+++ php/classes/author.php
@@ -1,6 +1,10 @@
 <?php
 namespace RussellDorgan\ObjectOriented;
+require_once("autoload.php");
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
+use Ramsey\Uuid\Uuid;
 class Author {
+	use ValidateUuid;
 	private $authorId;
 	private $authorActivationToken;
 	private $authorAvatarUrl;
@@ -9,46 +13,59 @@ class Author {
 	private $authorUsername;
 
 	public function __construct($newAuthorId, $newAuthorActivationToken, $newAuthorAvatarUrl, $newAuthorEmail, $newAuthorHash, $newAuthorUsername) {
-		$this->setAuthorId($newAuthorId);
-		$this->setAuthorActivationToken($newAuthorActivationToken);
-		$this->setAuthorAvatarUrl($newAuthorAvatarUrl);
-		$this->setAuthorEmail($newAuthorEmail);
-		$this->setAuthorHash($newAuthorHash);
-		$this->setAuthorUsername($newAuthorUsername);
 
+		try {
+			$this->setAuthorId($newAuthorId);
+			$this->setAuthorActivationToken($newAuthorActivationToken);
+			$this->setAuthorAvatarUrl($newAuthorAvatarUrl);
+			$this->setAuthorEmail($newAuthorEmail);
+			$this->setAuthorHash($newAuthorHash);
+			$this->setAuthorUsername($newAuthorUsername);
+		}
+			//determine what exception type was thrown
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
 	}
 
-		public function getAuthorId(){
+		public function getAuthorId() : Uuid {
 			return $this->authorId;
 		}
 		public function setAuthorId($newAuthorId){
-			$this->authorId = $newAuthorId;
+			try {
+				$uuid = self::validateUuid($newAuthorId);
+			} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+				$exceptionType = get_class($exception);
+				throw(new $exceptionType($exception->getMessage(), 0, $exception));
+			}
+			$this->authorId = $uuid;
 		}
-		public function getAuthorActivationToken(){
+		public function getAuthorActivationToken() : string {
 			return $this->authorActivationToken;
 		}
 		public function setAuthorActivationToken($newAuthorActivationToken){
 			$this->authorActivationToken = $newAuthorActivationToken;
 		}
-		public function getAuthorAvatarUrl(){
+		public function getAuthorAvatarUrl() : string {
 			return $this->authorAvatarUrl;
 		}
 		public function setAuthorAvatarUrl($newAuthorAvatarUrl){
 			$this->authorAvatarUrl = $newAuthorAvatarUrl;
 		}
-		public function getAuthorEmail(){
+		public function getAuthorEmail() : string {
 			return $this->authorEmail;
 		}
 		public function setAuthorEmail($newAuthorEmail){
 			$this->authorEmail = $newAuthorEmail;
 		}
-		public function getAuthorHash(){
+		public function getAuthorHash() : string {
 			return $this->authorHash;
 		}
 		public function setAuthorHash($newAuthorHash){
 			$this->authorHash = $newAuthorHash;
 		}
-		public function getAuthorUsername(){
+		public function getAuthorUsername() : string {
 			return $this->authorUsername;
 		}
 		public function setAuthorUsername($newAuthorUsername){
--- php/lib/author.php
+++ php/lib/author.php
@@ -5,4 +5,12 @@ require_once(dirname(__DIR__, 1) . "/classes/Author.php");
 //$secrets =  new Secrets("/etc/apache2/capstone-mysql/cohort28/cfiniello.ini");
 //$pdo = $secrets->getPdoObject();
 use RussellDorgan\ObjectOriented\Author;
-$author = new Author(newAuthorId, newAuthorActivationToken, newAuthorAvatarUrl, newAuthorEmail, newauthorHash, newAuthorUsername);
+$authorId = "006fb097-31bf-4ead-95a6-717c46e1c7f6";
+$authorActivationCode = "the_NEW_activation_code";
+$authorAvatarUrl = "https://avatar.org";
+$authorEmail = "myfakeemail@somewhere.com";
+$authorHash = "a_super_hash";
+$authorUsername = "thenewusername3";
+$author =  new Author($authorId, $authorActivationCode, $authorAvatarUrl, $authorEmail, $authorHash, $authorUsername);
+
+var_dump($author);
