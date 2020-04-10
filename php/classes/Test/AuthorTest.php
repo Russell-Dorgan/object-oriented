5ccb031f4900fb6649987e9c9075f9271ee47c05
Russell Dorgan <clambakedbenz@gmail.com>
2020-04-10 10:07:02 -0600

    Implemented a working Unit Test.

--- php/classes/Test/AuthorTest.php
+++ php/classes/Test/AuthorTest.php
@@ -35,10 +35,16 @@ class AuthorTest extends DataDesignTest {
 		$author = new Author($authorId, $this->VALID_ACTIVATION_TOKEN, $this->VALID_AVATAR_URL, $this->VALID_AUTHOR_EMAIL, $this->VALID_AUTHOR_HASH, $this->VALID_USERNAME);
 		$author->insert($this->getPDO());
 
-
 		$numRowsAfterInsert = $this->getConnection()->getRowCount("author");
 		self::assertEquals($numRows + 1, $numRowsAfterInsert, "insert checked record count");
 
+		$pdoAuthor = Author::getAuthorByAuthorId($this->getPDO(), $author->getAuthorId()->getBytes());
+		self::assertEquals($authorId, $pdoAuthor->getAuthorId());
+		self::assertEquals($this->VALID_ACTIVATION_TOKEN, $pdoAuthor->getauthorActivationToken());
+		self::assertEquals($this->VALID_AVATAR_URL, $pdoAuthor->getAuthorAvatarUrl());
+		self::assertEquals($this->VALID_AUTHOR_EMAIL, $pdoAuthor->getAuthorEmail());
+		self::assertEquals($this->VALID_AUTHOR_HASH, $pdoAuthor->getAuthorHash());
+		self::assertEquals($this->VALID_USERNAME, $pdoAuthor->getAuthorUsername());
 
 	}
 
